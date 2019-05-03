a07cb53515cf4f1f659cdc94311591a0576fd62f
joel vilca Alania <joel@idw.com.pe>
2019-05-03 00:34:00 -0500

    sin warnings

--- src/views/kairos/laboratories/KairoLaboratories.vue
+++ src/views/kairos/laboratories/KairoLaboratories.vue
@@ -1,44 +1,76 @@
 <template>
   <div>
-    <v-toolbar flat color="white">
-      <v-toolbar-title>Kairo Laboratorio</v-toolbar-title>
+    <v-toolbar
+      flat
+      color="white"
+    >
+      <v-toolbar-title>Kairo Laboratorios</v-toolbar-title>
       <v-divider
         class="mx-2"
         inset
         vertical
-      ></v-divider>
-      <v-spacer></v-spacer>
-      <v-dialog v-model="dialog" max-width="500px">
+      />
+      <v-spacer />
+      <v-dialog
+        v-model="dialog"
+        max-width="500px"
+      >
         <template v-slot:activator="{ on }">
-          <v-btn 
+          <v-btn
             color="primary"
             dark
-            class="mb-2" 
+            class="mb-2"
             v-on="on"
-          >Agregar Nuevo</v-btn>
+          >
+            Agregar Nuevo
+          </v-btn>
         </template>
         <v-card>
           <v-card-title>
             <span class="headline">{{ formTitle }}</span>
           </v-card-title>
-
           <v-card-text>
             <v-container grid-list-md>
               <v-layout wrap>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editedItem.name" label="Nombre"></v-text-field>
+                <v-flex
+                  xs12
+                  sm6
+                  md4
+                >
+                  <v-text-field
+                    v-model="editedItem.name"
+                    label="Dessert name"
+                  />
                 </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editedItem.state" label="Estado"></v-text-field>
+                <v-flex
+                  xs12
+                  sm6
+                  md4
+                >
+                  <v-text-field
+                    v-model="editedItem.state"
+                    label="State"
+                  />
                 </v-flex>
               </v-layout>
             </v-container>
           </v-card-text>
-
           <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="close">Cancelar</v-btn>
-            <v-btn color="blue darken-1" flat @click="save">Guardar</v-btn>
+            <v-spacer />
+            <v-btn
+              color="blue darken-1"
+              flat
+              @click="close"
+            >
+              Cancel
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              flat
+              @click="save"
+            >
+              Save
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -50,8 +82,12 @@
     >
       <template v-slot:items="props">
         <td>{{ props.item.name }}</td>
-        <td>{{ props.item.state }}</td>
-        <td>
+        <td
+          class="text-xs-right"
+        >
+          {{ props.item.state }}
+        </td>
+        <td class="justify-center layout px-0">
           <v-icon
             small
             class="mr-2"
@@ -68,89 +104,92 @@
         </td>
       </template>
       <template v-slot:no-data>
-        <v-btn color="primary">Vacio</v-btn>
+        <v-btn
+          color="primary"
+        >
+          Reset
+        </v-btn>
       </template>
     </v-data-table>
   </div>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex';
-  export default {
-    data: () => ({
-      dialog: false,
-      headers: [
-        {
-          text: 'Nombre',
-          align: 'left',
-          sortable: false,
-          value: 'name'
-        },
-        { text: 'Estado', value: 'state' },
-        { text: 'Actions', value: 'name', sortable: false }
-      ],
-      desserts: [],
-      editedIndex: -1,
-      editedItem: {
-        name: '',
-        state: 'activo',
+import { mapActions, mapState } from 'vuex'
+
+export default {
+
+  data: () => ({
+    dialog: false,
+    headers: [
+      {
+        text: 'Dessert (100g serving)',
+        align: 'left',
+        sortable: false,
+        value: 'name'
       },
-      defaultItem: {
-        name: '',
-        state: 'activo',
-      }
+      { text: 'State', value: 'state' },
+      { text: 'Actions', value: 'name', sortable: false }
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: '',
+      state: 'activo'
+    },
+    defaultItem: {
+      name: '',
+      state: 'activo'
+    }
+  }),
+
+  computed: {
+    ...mapState({
+      kairocompositions: state => state.kairoCompositions.kairocompositions,
+      loadingKairoCompositions: state => state.kairoCompositions.loadingKairoCompositions
     }),
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    }
+  },
 
-    computed: {
-      ...mapState({
-        laboratories: state => state.KairoLaboratories.laboratories,
-        loadingLaboratories: state => state.KairoLaboratories.loadingLaboratories
-      }),
-      formTitle () {
-        return this.editedIndex === -1 ? 'Nuevo Kairo-Laboratorio' : 'Editar Kairo-Laboratorio'
-      }
+  watch: {
+    dialog (val) {
+      val || this.close()
+    }
+  },
+
+  created () {
+    this.getLaboratories()
+  },
+
+  methods: {
+    ...mapActions({
+      getLaboratories: 'KairoLaboratories/getLaboratories'
+    }),
+    editItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
     },
-
-    watch: {
-      dialog (val) {
-        val || this.close()
-      }
+    deleteItem (item) {
+      const index = this.desserts.indexOf(item)
+      confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
     },
-
-    created () {
-      this.getLaboratories()
+    close () {
+      this.dialog = false
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      }, 300)
     },
-
-    methods: {
-      ...mapActions({
-        getLaboratories: 'KairoLaboratories/getLaboratories'
-      }),
-      editItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
-
-      deleteItem (item) {
-        const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
-      },
-
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
-        }
-        this.close()
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        this.desserts.push(this.editedItem)
       }
+      this.close()
     }
   }
+}
 </script>
